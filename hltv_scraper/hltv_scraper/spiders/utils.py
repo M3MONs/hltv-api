9869c1b8a710df8f8220bc4710673815a9291ac8
M3MONs <alan.skrrrz@gmail.com>
2024-03-19 19:56:49 +0100

    edit: rounds -> score in parse_team

--- hltv_scraper/hltv_scraper/spiders/utils.py
+++ hltv_scraper/hltv_scraper/spiders/utils.py
@@ -1,7 +1,7 @@
 def parse_team(result, number):
     return {
         "name": result.css(f"div.team{number} .team::text").get(),
-        "rounds": result.css(f"td.result-score span:nth-child({number})::text").get(),
+        "score": result.css(f"td.result-score span:nth-child({number})::text").get(),
         "logo": result.css(f"div.team{number} img::attr(src)").get(),
     }
 
