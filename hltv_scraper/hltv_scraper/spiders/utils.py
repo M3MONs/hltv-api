1d75dd14f6461dbd3d13cec944a6bf3065904410
M3MONs <alan.skrrrz@gmail.com>
2024-03-19 19:56:19 +0100

    add: parse_match function

--- hltv_scraper/hltv_scraper/spiders/utils.py
+++ hltv_scraper/hltv_scraper/spiders/utils.py
@@ -4,3 +4,12 @@ def parse_team(result, number):
         "rounds": result.css(f"td.result-score span:nth-child({number})::text").get(),
         "logo": result.css(f"div.team{number} img::attr(src)").get(),
     }
+
+
+def parse_match(result):
+    return {
+        "map": result.css("div.map-text::text").get(),
+        "event": result.css("span.event-name::text").get(),
+        "team1": parse_team(result, 1),
+        "team2": parse_team(result, 2),
+    }
