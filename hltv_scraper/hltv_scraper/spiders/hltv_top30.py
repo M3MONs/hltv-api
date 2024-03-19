f0c3dedf74fcace6c7ac87e75fa752d5227fe6e4
M3MONs <alan.skrrrz@gmail.com>
2024-03-19 20:01:18 +0100

    add: import parse_team_ranking

--- hltv_scraper/hltv_scraper/spiders/hltv_top30.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_top30.py
@@ -1,4 +1,5 @@
 import scrapy
+from .utils import parse_team_ranking
 
 
 class HltvTop30Spider(scrapy.Spider):
@@ -10,19 +11,6 @@ class HltvTop30Spider(scrapy.Spider):
         ranked_teams = response.css("div.ranked-team.standard-box")
 
         for team in ranked_teams:
-            position = team.css("span.position::text").get()
-            name = team.css(".name::text").get()
-            logo = team.css("span.team-logo img::attr(src)").get()
-            points = team.css("span.points::text").get()
-            players = team.css("div.playersLine .rankingNicknames span::text").getall()
-
-            data = {
-                "position": position if position else None,
-                "name": name.strip() if name else None,
-                "logo": logo.strip() if logo else None,
-                "points": points.strip() if points else None,
-                "players": players,
-            }
-
+            data = parse_team_ranking(team)
             # print(data)
             yield data
