1c5995e467c1bf214fc0b2dda5b175033e7710b5
M3MONs <alan.skrrrz@gmail.com>
2024-03-19 19:49:18 +0100

    add: mv parse_team to utils

--- hltv_scraper/hltv_scraper/spiders/hltv_results.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_results.py
@@ -1,4 +1,5 @@
 import scrapy
+from .utils import parse_team
 
 
 class HltvResultsSpider(scrapy.Spider):
@@ -7,21 +8,12 @@ class HltvResultsSpider(scrapy.Spider):
     start_urls = ["https://www.hltv.org/results?offset=0"]
     base_api_url = "https://www.hltv.org/results?offset={}"
 
-    def parse_team(self, result, number):
-        return {
-            "name": result.css(f"div.team{number} .team::text").get(),
-            "rounds": result.css(
-                f"td.result-score span:nth-child({number})::text"
-            ).get(),
-            "logo": result.css(f"div.team{number} img::attr(src)").get(),
-        }
-
     def parse_match(self, result):
         return {
             "map": result.css("div.map-text::text").get(),
             "event": result.css("span.event-name::text").get(),
-            "team1": self.parse_team(result, 1),
-            "team2": self.parse_team(result, 2),
+            "team1": parse_team(result, 1),
+            "team2": parse_team(result, 2),
         }
 
     def parse_results(self, sublists):
