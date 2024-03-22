4d80a705cb6ebbbd768c6203cc140ccc6cc20fb1
M3MONs <alan.skrrrz@gmail.com>
2024-03-22 16:40:15 +0100

    ref: spider accept team name as argument

--- hltv_scraper/hltv_scraper/spiders/hltv_team.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_team.py
@@ -1,3 +1,4 @@
+from typing import Any
 import scrapy
 
 
@@ -5,7 +6,9 @@ class HltvTeamSpider(scrapy.Spider):
     name = "hltv_team"
     allowed_domains = ["www.hltv.org"]
     start_urls = ["https://www.hltv.org/search?query="]
-    query = "ENCE"
+
+    def __init__(self, team_name="ENCE", **kwargs: Any):
+        super().__init__(**kwargs)
 
     def parse(self, response):
         pass
