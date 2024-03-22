0258f3a559685c4848ab76841894b7632dd1a890
M3MONs <alan.skrrrz@gmail.com>
2024-03-22 16:44:06 +0100

    ref: start_urls initialization in HltvTeamSpider constructor

--- hltv_scraper/hltv_scraper/spiders/hltv_team.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_team.py
@@ -5,9 +5,9 @@ import scrapy
 class HltvTeamSpider(scrapy.Spider):
     name = "hltv_team"
     allowed_domains = ["www.hltv.org"]
-    start_urls = ["https://www.hltv.org/search?query="]
 
     def __init__(self, team_name="ENCE", **kwargs: Any):
+        self.start_urls = [f"https://www.hltv.org/search?query={team_name}"]
         super().__init__(**kwargs)
 
     def parse(self, response):
