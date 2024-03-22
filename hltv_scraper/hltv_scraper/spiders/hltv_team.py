2e3c90779309d74b1b299847d54db16de9ba5a19
M3MONs <alan.skrrrz@gmail.com>
2024-03-22 12:05:01 +0100

    add: default_query ENCE

--- hltv_scraper/hltv_scraper/spiders/hltv_team.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_team.py
@@ -5,6 +5,7 @@ class HltvTeamSpider(scrapy.Spider):
     name = "hltv_team"
     allowed_domains = ["www.hltv.org"]
     start_urls = ["https://www.hltv.org/search?query="]
+    query = "ENCE"
 
     def parse(self, response):
         pass
