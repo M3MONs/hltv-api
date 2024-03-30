e7c7da5d2ecabdaaa6bcdf24fb2076069651d09c
M3MONs <alan.skrrrz@gmail.com>
2024-03-30 12:29:46 +0100

    add: spider returns a link to the team profile if it is not null

--- hltv_scraper/hltv_scraper/spiders/hltv_teams_id.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_teams_id.py
@@ -1,10 +1,34 @@
-import scrapy
+import scrapy, json, os
+from typing import Any
 
 
 class HltvTeamsIdSpider(scrapy.Spider):
     name = "hltv_teams_id"
     allowed_domains = ["www.hltv.org"]
-    start_urls = ["https://www.hltv.org/search?query="]
+
+    def __init__(self, team_name="", **kwargs: Any):
+        self.start_urls = [f"https://www.hltv.org/search?query={team_name}"]
+        self.team_name = team_name.replace(" ", "-")
+        super().__init__(**kwargs)
+
+    def get_profile_link(self, response):
+        return response.css(
+            f"a[href^='/team/'][href$='/{self.team_name}']::attr(href)"
+        ).get()
+
+    def save_to_json(self, data):
+        existing_data = {}
+
+        if os.path.exists("teams_profile.json"):
+            with open("teams_profile.json", "r") as json_file:
+                existing_data = json.load(json_file)
+
+        existing_data.update(data)
+
+        with open("teams_profile.json", "w") as json_file:
+            json.dump(existing_data, json_file, indent=4)
 
     def parse(self, response):
-        pass
+        profile_link = self.get_profile_link(response)
+        if profile_link:
+            self.save_to_json({f"{self.team_name}": profile_link})
