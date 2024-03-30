77e82f0dbe16cb42f7fff448a543631fda6051c3
M3MONs <alan.skrrrz@gmail.com>
2024-03-30 13:22:22 +0100

    add: checks if there is a profile link and then generates and sends the team json

--- app.py
+++ app.py
@@ -30,17 +30,31 @@ def run_spider(spider_name: str, json_name: str):
     process.wait()
 
 
-def run_team_spider(spider_name: str, json_name: str, name: str):
-    clear_old_data(json_name)
+def run_team_profile_spider(spider_name: str, name: str):
     process = subprocess.Popen(
         [
             "scrapy",
             "crawl",
             spider_name,
-            "-o",
-            f"{json_name}.json",
             "-a",
-            f"team_name={name}",
+            f"team={name}",
+        ],
+        cwd="./hltv_scraper",
+    )
+    process.wait()
+
+
+def run_team_spider(spider_name: str, name: str, profile: str):
+
+    process = subprocess.Popen(
+        [
+            "scrapy",
+            "crawl",
+            spider_name,
+            "-a",
+            f"team={profile}",
+            "-o",
+            f"{name}.json",
         ],
         cwd="./hltv_scraper",
     )
@@ -103,16 +117,44 @@ def upcoming_matches():
     return jsonify(data)
 
 
+def is_team_profile_link(team_name: str):
+    profiles_json = "./hltv_scraper/teams_profile.json"
+
+    if not os.path.exists(profiles_json):
+        return False
+
+    with open(profiles_json) as file:
+        profiles = json.load(file)
+        if team_name in profiles:
+            return True
+
+    return False
+
+
+def get_profile_link(team_name: str):
+    profiles_json = "./hltv_scraper/teams_profile.json"
+    with open(profiles_json) as file:
+        profiles = json.load(file)
+        return profiles[team_name]
+
+
 @app.route("/team/<name>", methods=["GET"])
 @limiter.limit("1 per second")
 def team(name: str):
-    spider_name = "hltv_team"
-    json_name = name
+    spider_name = "hltv_teams_id"
+    spider_name2 = "hltv_team"
 
-    if should_run_spider(json_name, 24):
-        run_team_spider(spider_name, json_name, name)
+    if not is_team_profile_link(name.lower()):
+        run_team_profile_spider(spider_name, name)
 
-    with open(f"./hltv_scraper/{json_name}.json", "r") as file:
+    if not is_team_profile_link(name.lower()):
+        return "Team not found!"
+
+    profile_link = get_profile_link(name.lower())
+    if should_run_spider(name.lower(), 24):
+        run_team_spider(spider_name2, name.lower(), profile_link)
+
+    with open(f"./hltv_scraper/{name.lower()}.json", "r") as file:
         data = json.load(file)
 
     return jsonify(data)
