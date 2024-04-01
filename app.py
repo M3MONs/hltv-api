49d701a3a63c734736c7c5f9a8fa5fffc1e0662d
M3MONs <alan.skrrrz@gmail.com>
2024-04-01 16:06:37 +0200

    ref: move non-route functions to utils.py file

--- app.py
+++ app.py
@@ -1,64 +1,16 @@
 from flask import Flask, jsonify
 from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-import os, time, json, subprocess
+from utils import (
+    should_run_spider,
+    run_spider,
+    run_team_profile_spider,
+    run_team_spider,
+)
+import os, json
 
 app = Flask(__name__)
 
 limiter = Limiter(app, default_limits=["1 per second"])
-
-
-def should_run_spider(json_name: str, hours: int = 1):
-    localization = f"./hltv_scraper/{json_name}.json"
-    if os.path.exists(localization) and time.time() - os.path.getmtime(localization) < (
-        3600 * hours
-    ):
-        return False
-    return True
-
-
-def clear_old_data(json_name: str):
-    open(f"./hltv_scraper/{json_name}.json", "w").close()
-
-
-def run_spider(spider_name: str, json_name: str):
-    clear_old_data(json_name)
-    process = subprocess.Popen(
-        ["scrapy", "crawl", spider_name, "-o", f"{json_name}.json"],
-        cwd="./hltv_scraper",
-    )
-    process.wait()
-
-
-def run_team_profile_spider(spider_name: str, name: str):
-    process = subprocess.Popen(
-        [
-            "scrapy",
-            "crawl",
-            spider_name,
-            "-a",
-            f"team={name}",
-        ],
-        cwd="./hltv_scraper",
-    )
-    process.wait()
-
-
-def run_team_spider(spider_name: str, name: str, profile: str):
-
-    process = subprocess.Popen(
-        [
-            "scrapy",
-            "crawl",
-            spider_name,
-            "-a",
-            f"team={profile}",
-            "-o",
-            f"{name}.json",
-        ],
-        cwd="./hltv_scraper",
-    )
-    process.wait()
 
 
 @app.route("/results", methods=["GET"])
