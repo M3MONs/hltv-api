d7bbb99b20491639f3e1498f625ba565615548a8
M3MONs <alan.skrrrz@gmail.com>
2024-04-01 16:58:39 +0200

    ref: consolidate run_spider for universal use

--- utils.py
+++ utils.py
@@ -14,41 +14,12 @@ def clear_old_data(json_name: str):
     open(f"./hltv_scraper/{json_name}.json", "w").close()
 
 
-def run_spider(spider_name: str, json_name: str):
-    clear_old_data(json_name)
+def run_spider(spider_name: str, json_name: str, args: str):
+    if os.path.exists(f"./hltv_scraper/{json_name}.json"):
+        clear_old_data(json_name)
+
     process = subprocess.Popen(
-        ["scrapy", "crawl", spider_name, "-o", f"{json_name}.json"],
-        cwd="./hltv_scraper",
-    )
-    process.wait()
-
-
-def run_team_profile_spider(spider_name: str, name: str):
-    process = subprocess.Popen(
-        [
-            "scrapy",
-            "crawl",
-            spider_name,
-            "-a",
-            f"team={name}",
-        ],
-        cwd="./hltv_scraper",
-    )
-    process.wait()
-
-
-def run_team_spider(spider_name: str, name: str, profile: str):
-    clear_old_data(name)
-    process = subprocess.Popen(
-        [
-            "scrapy",
-            "crawl",
-            spider_name,
-            "-a",
-            f"team={profile}",
-            "-o",
-            f"{name}.json",
-        ],
+        ["scrapy", "crawl", spider_name] + args.split(),
         cwd="./hltv_scraper",
     )
     process.wait()
