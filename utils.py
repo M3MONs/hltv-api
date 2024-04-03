8655063b69cb03722b27629f60a45900bbe43697
M3MONs <alan.skrrrz@gmail.com>
2024-04-03 17:19:40 +0200

    ref: Implement BASE_DIR variable for directory handling

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import os, time, subprocess, json
 
+BASE_DIR = "./hltv_scraper"
+
 
 def should_run_spider(json_name: str, hours: int = 1):
-    localization = f"./hltv_scraper/{json_name}.json"
+    localization = os.path.join(BASE_DIR, f"{json_name}.json")
     if os.path.exists(localization) and time.time() - os.path.getmtime(localization) < (
         3600 * hours
     ):
@@ -16,13 +18,13 @@ def load_json_data(filename: str):
 
 
 def is_profile_link(filename: str, profile: str):
-    json_file = f"./hltv_scraper/{filename}.json"
+    file = os.path.join(BASE_DIR, f"{filename}.json")
 
-    if not os.path.exists(json_file):
+    if not os.path.exists(file):
         return False
 
-    with open(json_file) as file:
-        profiles = json.load(file)
+    with open(file) as f:
+        profiles = json.load(f)
         if profile in profiles:
             return True
 
@@ -30,22 +32,22 @@ def is_profile_link(filename: str, profile: str):
 
 
 def get_profile_data(filename: str, profile: str):
-    profiles_json = f"./hltv_scraper/{filename}.json"
-    with open(profiles_json) as file:
-        profiles = json.load(file)
+    file = os.path.join(BASE_DIR, f"{filename}.json")
+    with open(file) as f:
+        profiles = json.load(f)
         return profiles[profile]
 
 
 def clear_old_data(json_name: str):
-    open(f"./hltv_scraper/{json_name}.json", "w").close()
+    open(os.path.join(BASE_DIR, f"{json_name}.json"), "w").close()
 
 
 def run_spider(spider_name: str, json_name: str, args: str):
-    if os.path.exists(f"./hltv_scraper/{json_name}.json"):
+    if os.path.exists(os.path.join(BASE_DIR, f"{json_name}.json")):
         clear_old_data(json_name)
 
     process = subprocess.Popen(
         ["scrapy", "crawl", spider_name] + args.split(),
-        cwd="./hltv_scraper",
+        cwd=BASE_DIR,
     )
     process.wait()
