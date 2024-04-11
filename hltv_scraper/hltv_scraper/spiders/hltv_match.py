fec496eafe61d5505230e9c508b516f3071320da
M3MONs <alan.skrrrz@gmail.com>
2024-04-11 17:06:42 +0200

    add: init start_urls with match param

--- hltv_scraper/hltv_scraper/spiders/hltv_match.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_match.py
@@ -1,10 +1,14 @@
+from typing import Any
 import scrapy
 
 
 class HltvMatchSpider(scrapy.Spider):
     name = "hltv_match"
     allowed_domains = ["www.hltv.org"]
-    start_urls = ["https://www.hltv.org/matches"]
+
+    def __init__(self, match: str, **kwargs: Any):
+        self.start_urls = [f"https://www.hltv.org/matches/{match}"]
+        super().__init__(**kwargs)
 
     def parse(self, response):
         pass
