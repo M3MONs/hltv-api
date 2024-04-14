2f89b11fc395af924048fb4134f3ffd256d67891
M3MONs <alan.skrrrz@gmail.com>
2024-04-14 17:28:12 +0200

    add: complete hltv_team_matches spider

--- hltv_scraper/hltv_scraper/spiders/hltv_team_matches.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_team_matches.py
@@ -1,10 +1,17 @@
+from typing import Any
 import scrapy
+from .utils import parse_team_results
 
 
 class HltvTeamMatchesSpider(scrapy.Spider):
     name = "hltv_team_matches"
     allowed_domains = ["www.hltv.org"]
-    start_urls = ["https://www.hltv.org/results?team="]
+
+    def __init__(self, id: str, **kwargs: Any):
+        self.start_urls = [f"https://www.hltv.org/results?team={id}"]
+        super().__init__(**kwargs)
 
     def parse(self, response):
-        pass
+        results = response.css(".results-all .result")
+        data = parse_team_results(results)
+        yield from data
