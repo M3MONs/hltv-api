d3ed9de2d2ae12ad6505110f24ce29d611245689
M3MONs <alan.skrrrz@gmail.com>
2024-04-16 13:41:27 +0200

    add: offset param to init start_urls in spider

--- hltv_scraper/hltv_scraper/spiders/hltv_results.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_results.py
@@ -1,3 +1,4 @@
+from typing import Any
 import scrapy
 from .utils import parse_results
 
@@ -5,8 +6,10 @@ from .utils import parse_results
 class HltvResultsSpider(scrapy.Spider):
     name = "hltv_results"
     allowed_domains = ["www.hltv.org"]
-    start_urls = ["https://www.hltv.org/results?offset=0"]
-    base_api_url = "https://www.hltv.org/results?offset={}"
+
+    def __init__(self, offset: int, **kwargs: Any):
+        self.start_urls = [f"https://www.hltv.org/results?offset={offset}"]
+        super().__init__(**kwargs)
 
     def parse(self, response):
         sublists = response.css("div.allres .results-sublist")
