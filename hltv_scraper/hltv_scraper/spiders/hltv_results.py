90eef8ba07c17204dfae809db8efa8e18b72ba28
M3MONs <alan.skrrrz@gmail.com>
2024-04-16 13:45:09 +0200

    add: default offset value 0 if not passed

--- hltv_scraper/hltv_scraper/spiders/hltv_results.py
+++ hltv_scraper/hltv_scraper/spiders/hltv_results.py
@@ -7,7 +7,7 @@ class HltvResultsSpider(scrapy.Spider):
     name = "hltv_results"
     allowed_domains = ["www.hltv.org"]
 
-    def __init__(self, offset: int, **kwargs: Any):
+    def __init__(self, offset: int = 0, **kwargs: Any):
         self.start_urls = [f"https://www.hltv.org/results?offset={offset}"]
         super().__init__(**kwargs)
 
