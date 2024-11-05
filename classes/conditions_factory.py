cf873a7861637faa99b6e92ab80246dca7525fa5
M3MONs <67465063+M3MONs@users.noreply.github.com>
2024-11-05 21:57:58 +0100

    feat: add support for FileExistsCondition in ConditionFactory

--- classes/conditions_factory.py
+++ classes/conditions_factory.py
@@ -1,4 +1,4 @@
-from .conditions import Condition, FileTimeCondition, JsonFileEmptyCondition
+from .conditions import Condition, FileTimeCondition, JsonFileEmptyCondition, FileExistsCondition
 
 class ConditionFactory:
     @staticmethod
@@ -7,5 +7,7 @@ class ConditionFactory:
             return FileTimeCondition()
         elif condition_type == 'json_file_empty':
             return JsonFileEmptyCondition()
+        elif condition_type == 'file_exists':
+            return FileExistsCondition()
         else:
             raise ValueError(f"Unknown condition type: {condition_type}")
