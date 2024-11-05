528b5b51b7515ef963ca9bde13fb0ba1137d9a4c
M3MONs <67465063+M3MONs@users.noreply.github.com>
2024-11-05 22:24:15 +0100

    feat: add SpiderProcess to module exports

--- classes/__init__.py
+++ classes/__init__.py
@@ -3,6 +3,7 @@ from .data import JsonDataLoader
 from .path_generator import JsonFilePathGenerator
 from .conditions_checker import ConditionsChecker
 from .conditions_factory import ConditionFactory
+from .process import SpiderProcess
 
 __all__ = [
     "JsonOldDataCleaner",
@@ -10,4 +11,5 @@ __all__ = [
     "JsonFilePathGenerator",
     "ConditionsChecker",
     "ConditionFactory",
+    "SpiderProcess",
 ]
