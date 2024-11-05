157e6369605026963106c28c20b22c50e1f2b1b9
M3MONs <67465063+M3MONs@users.noreply.github.com>
2024-11-05 21:43:21 +0100

    refactor: update imports in __init__.py to use ConditionsChecker and ConditionsFactory

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,13 +1,13 @@
 from .cleaner import JsonOldDataCleaner
 from .data import JsonDataLoader
 from .path_generator import JsonFilePathGenerator
-from .spider_run_checker import SpiderRunChecker
-from .condition_factory import ConditionFactory
+from .conditions_checker import ConditionsChecker
+from .conditions_factory import ConditionFactory
 
 __all__ = [
     "JsonOldDataCleaner",
     "JsonDataLoader",
     "JsonFilePathGenerator",
-    "SpiderRunChecker",
+    "ConditionsChecker",
     "ConditionFactory",
 ]
